488be9aa4dc665f3c4302babb68a26700f15d568
smjprincy <39133521+smjprincy@users.noreply.github.com>
2018-05-09 23:18:12 +0530

    Update AccountMapController.js

--- AccountMapController.js
+++ AccountMapController.js
@@ -3,21 +3,19 @@
         var map = L.map('map', {zoomControl: false, tap: false},14);
         L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
             {
-                attribution: 'Tiles © Esri'
+                attribution: 'Tiles Â© Esri'
             }).addTo(map);
         function onLocationFound(e) {
         var radius = e.accuracy / 2;
 		alert(e.latlng);
-           // //var mark1=component.get('v.mark');
+          
             var circ1=component.get('v.circ');
-            //map.removeLayer(mark1);
-           // map.removeLayer(circ1);
+          
         var mark=L.marker(e.latlng).addTo(map)
             .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
         var circ=L.circle(e.latlng, radius).addTo(map);
-            //component.set('v.mark',mark);
-           // component.set('v.circ',circ);
+      
     }
 
     function onLocationError(e) {
